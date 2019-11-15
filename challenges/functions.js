d10568dd2533d06976a385620b43894258a39cd6
adastraz <tylergosox@gmail.com>
2019-11-15 11:15:18 -0500

    Finished functions.js

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,19 +6,29 @@
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
-
+const consume = function (a,b,cb){
+  return cb(a,b)
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
-
+const add = function (a,b){
+  return a+b
+}
+const multiply = function (a,b){
+  return a*b 
+} 
+const greeting = function (a,b){
+  return `Hello ${a} ${b}, nice to meet you!`
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -27,6 +37,7 @@
 
 // Explanation: 
 
+// So we can see that nested function is inside of the function myFunction, which is allowed to be called by the 'parent' function myFunction because it is in the same Scope. if you were outside of the function myFunction and tried to call internal it would give back an error.  
 
 const external = "I'm outside the function";
 
